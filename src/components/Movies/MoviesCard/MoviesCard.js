62a996e0586d8feb08fbd8d43eca6eb0253d9a18
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-09-12 21:55:26 +0300

    cards add to /saved-movies

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './MoviesCard.css';
 import MoviesApi from '../../../utils/MoviesApi';
 
@@ -6,6 +7,17 @@ function Movies() {
   const [movies, setMovies] = useState([]);
   const [favorites, setFavorites] = useState([]);
   const [visibleMovies, setVisibleMovies] = useState(12);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    setFavorites(storedFavorites);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  }, [favorites]);
 
   useEffect(() => {
     MoviesApi.getMovies().then(result => {
@@ -14,17 +26,15 @@ function Movies() {
   }, []);
 
   const addToFavorites = (movie) => {
-    setFavorites([...favorites, movie]);
+    const updatedFavorites = [...favorites, movie];
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
   };
 
   const removeFromFavorites = (movie) => {
-    const indexToRemove = favorites.findIndex((item) => item._id === movie._id);
-
-    if (indexToRemove !== -1) {
-      const updatedFavorites = [...favorites];
-      updatedFavorites.splice(indexToRemove, 1);
-      setFavorites(updatedFavorites);
-    }
+    const updatedFavorites = favorites.filter((item) => item._id !== movie._id);
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
   };
 
   const loadMoreMovies = () => {
@@ -33,32 +43,35 @@ function Movies() {
 
   return (
     <>
-      {movies.slice(0, visibleMovies).map((movie) => (
-        <div className="movies-card" key={movie._id}>
-          <div className="movies-card__about">
-            <h2 className="movies-card__title">{movie.nameRU}</h2>
-            <p className='movies-card__duration'>{movie.duration}</p>
+      {location.pathname === '/movies' && (
+        movies.slice(0, visibleMovies).map((movie) => (
+          <div className="movies-card" key={movie._id}>
+            <div className="movies-card__about">
+              <h2 className="movies-card__title">{movie.nameRU}</h2>
+              <p className='movies-card__duration'>{movie.duration}</p>
+            </div>
+            <img className='movies-card__image' src={'https://api.nomoreparties.co' + movie.image.url} alt={movie.nameRU} />
+            <button className='movies-card__add movies-card__add_active' onClick={() => addToFavorites(movie)}>Добавить в избранное</button>
           </div>
-          <img className='movies-card__image' src={'https://api.nomoreparties.co' + movie.image.url} alt={movie.nameRU} />
-          <button className='movies-card__add movies-card__add_active' onClick={() => addToFavorites(movie)}>Добавить в избранное</button>
-        </div>
-      ))}
+        ))
+      )}
 
-      {visibleMovies < movies.length && (
+      {location.pathname === '/movies' && visibleMovies < movies.length && (
         <button onClick={loadMoreMovies}>Загрузить еще</button>
       )}
 
-      {favorites.map((favorite) => (
-        <div className="movies-card">
-
-          <div className="movies-card__about" key={favorite._id}>
-            <h2 className='movies-card__title'>{favorite.nameRU}</h2>
-            <p className='movies-card__duration'>{favorite.duration}</p>
+      {location.pathname === '/saved-movies' && (
+        favorites.map((favorite) => (
+          <div className="movies-card" key={favorite._id}>
+            <div className="movies-card__about">
+              <h2 className='movies-card__title'>{favorite.nameRU}</h2>
+              <p className='movies-card__duration'>{favorite.duration}</p>
+            </div>
+            <img src={'https://api.nomoreparties.co' + favorite.image.url} alt={favorite.nameRU} />
+            <button className='movies-card__add movies-card__add_delete' onClick={() => removeFromFavorites(favorite)}></button>
           </div>
-          <img src={'https://api.nomoreparties.co' + favorite.image.url} alt={favorite.nameRU} />
-          <button className='movies-card__add movies-card__add_delete' onClick={() => removeFromFavorites(favorite)}>Удалить из избранного</button>
-        </div>
-      ))}
+        ))
+      )}
     </>
   );
 }
