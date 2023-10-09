c5949d4f69ac1406d0f8eb6565ca605befd9ef8c
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-10-09 12:15:24 +0300

    clean MoviesCard

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -28,53 +28,6 @@ const MoviesCard = ({ favorites, addToFavorites, removeFromFavorites, movie }) =
 
       })
     }
-  };
-
-  // console.trace('filteredMovies', filteredMovies);
-  // return (
-  //   <>
-  //     {filteredMovies
-  //       .slice(0, visibleMovies)
-  //       .map((movie) => (
-  //         <div className="movies-card" key={movie._id}>
-  //           <div className="movies-card__about">
-  //             <h2 className="movies-card__title">{movie.nameRU}</h2>
-  //             <p className="movies-card__duration">{movie.duration}</p>
-  //           </div>
-  //           <img
-  //             className="movies-card__image"
-  //             src={movie.image.url ? 'https://api.nomoreparties.co' + movie.image.url : movie.image}
-  //             alt={movie.nameRU}
-  //           />
-  //           {location.pathname === '/movies' && (
-  //             <button
-  //               className={`movies-card__add ${favorites.some((favMovie) => {
-  //                 return favMovie.movieId === movie.movieId
-  //               })
-  //                   ? 'movies-card__add_active'
-  //                   : ''
-  //                 }`}
-  //               onClick={() => handleCheckboxClick(movie)}
-  //             >
-  //               Сохранить
-  //             </button>
-  //           )}
-
-  //           {location.pathname === '/saved-movies' && favorites.some((favMovie) => {
-  //             console.log(favMovie.movieId, movie.movieId, favMovie.id === movie.movieId, movie.movieId);
-  //             return favMovie.movieId === movie.movieId
-  //           }) ? (
-  //             <button
-  //               className="movies-card__add movies-card__add_delete"
-  //               onClick={() => handleCheckboxClick(movie)}
-  //             >
-  //               Удалить из избранного
-  //             </button>
-  //           ) : null}
-  //         </div>
-  //       ))}
-  //   </>
-  // );
 
 
   return (
