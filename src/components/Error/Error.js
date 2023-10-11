e8bf60d8b265e5b8c34cf2702d38fd8f214e15f6
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-10-11 12:16:41 +0300

    go back

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,12 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Error.css';
 
 function Error() {
+  const history = useHistory(); 
+
+  const goBack = () => {
+    history.goBack();
+  }
+
   return (
     <main className='error'>
       <h2 className='error__title'>404</h2>
-      <p className='error__text'>Страница не найдена</p>
-      <Link className='error__link' to='/'>Назад</Link>
+      <p className='error__text'>Page not found</p>
+      <button className='error__link' onClick={goBack}>Go back</button>
     </main>
   )
 }
