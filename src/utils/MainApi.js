74b921923b5bd7f5ba1fbf8d4e905759b6606e3e
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-10-12 16:37:43 +0300

    fix url

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -119,8 +119,8 @@ class Api {
 }
 
 const mainApi = new Api({
-  baseUrl: "http://localhost:8080",
-  //baseUrl: "https://api.deploy-diploma.nomoreparties.co",
+  //baseUrl: "http://localhost:8080",
+  baseUrl: "https://api.deploy-diploma.nomoreparties.co",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("jwt")}`,
